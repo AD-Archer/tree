8d4ce91f3f23b991a2547680321d0a259e4fffdd
ad-archer <antonioarcher.dev@gmail.com>
2025-12-28 08:23:04 -0500

    Add Plausible Analytics integration and update main.jsx formatting
    - Integrated Plausible Analytics for tracking events, outbound links, file downloads, and form submissions.
    - Reformatted import statements and rendering method in main.jsx for consistency.
    - Updated resume.pdf with new content.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/custom.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/custom.css";
+import App from "./App.jsx";
+import { init } from "@plausible-analytics/tracker";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+init({
+  domain: "adarcher.app",
+  endpoint: "https://plausible.adarcher.app/api/event",
+  outboundLinks: true,
+  fileDownloads: true,
+  formSubmissions: true,
+});
